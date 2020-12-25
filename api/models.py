34a63bb47101fc700edfa5550febff94a46dc20e
Praneeth Karnena <praneeth.codes@gmail.com>
2020-12-25 21:52:08 +0530

    Add base model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True, editable=False, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
