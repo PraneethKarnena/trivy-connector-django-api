3081b6afd5be6e630e772271eb3b91bcd626b227
Praneeth Karnena <praneeth.codes@gmail.com>
2020-12-26 00:14:55 +0530

    Implement scan result view

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ from api import views
 
 urlpatterns = [
     path('scan/', views.ScanView.as_view(), name='scan'),
+    path('result/<uuid:pk>/', views.ScanResultView.as_view(), name='result'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
 from rest_framework import generics
 
 from api.serializers import ScanResultSerializer
+from api.models import ScanResult
 
 
 class ScanView(generics.CreateAPIView):
+    serializer_class = ScanResultSerializer
+
+
+class ScanResultView(generics.RetrieveAPIView):
+    queryset = ScanResult.objects.all()
     serializer_class = ScanResultSerializer
     
