921e7109930ca5418cf65628989fc3fc1951c528
Praneeth Karnena <praneeth.codes@gmail.com>
2020-12-25 21:21:06 +0530

    Add comment

--- trivy_connector_django_api/settings.py
+++ trivy_connector_django_api/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Other apps
     'rest_framework',
 
     # Project apps
